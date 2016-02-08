c502aa4608235443dbc135f507e35994526a596d
Matthew Butler <butler.matthew@gmail.com>
2016-02-08 12:41:29 -0400

    Fix time on events
    * properly display string not secondsSinceEpoch

--- lib/src/zabbix_event.dart
+++ lib/src/zabbix_event.dart
@@ -12,7 +12,8 @@ class ZabbixEvent extends ZabbixChild {
   static const String isType = 'zabbixEventNode';
   static Map<String, dynamic> definition(Map event) {
     final sources = ['trigger', 'discovery rule', 'auto-registration', 'internal'];
-    var source = sources[int.parse(event['sources'])];
+    var source = sources[int.parse(event['source'])];
+    var time = new DateTime.fromMillisecondsSinceEpoch(int.parse(event['clock']) * 1000);
 
     final objects = {
       '0' : { '0' : 'Trigger'},
@@ -34,13 +35,39 @@ class ZabbixEvent extends ZabbixChild {
     };
     var eventValue = values[event['source']][event['value']];
 
-    return {
+    var acknowledgements = { };
+    for (Map ack in event['acknowledges']) {
+      var ackTime = new DateTime.fromMillisecondsSinceEpoch(int.parse(ack['clock']) * 1000);
+
+      acknowledgements[ack['acknowledgeid']] = {
+        'userid' : {
+          r'$name' : 'User ID',
+          r'$type' : 'string',
+          r'?value' : ack['userid'],
+          'alias' : ZabbixValue.definition('Alias', 'string', ack['alias'],
+              false),
+          'name' : ZabbixValue.definition('Name', 'string', ack['name'], false),
+          'surname' : ZabbixValue.definition('Surname', 'string',
+              ack['surname'], false)
+        },
+        'clock' : ZabbixValue.definition('Acknowledged Time', 'string',
+            ackTime.toIso8601String(), false),
+        'message' : ZabbixValue.definition('Message', 'string', ack['message'],
+            false)
+      };
+    }
+
+    var ret = {
       r'$is' : isType,
       'eventid' : ZabbixValue.definition('Event ID', 'string', event['eventid'],
           false),
-      'acknowledged' : ZabbixValue.definition('Acknowledged', 'number',
-          event['acknowledged'], false),
-      'clock' : ZabbixValue.definition('Created', 'string', event['clock'],
+      'acknowledged' : {
+        r'$name' : 'Acknowledged',
+        r'$type' : 'number',
+        r'?value' : int.parse(event['acknowledged']),
+        'Acknowledgements' : acknowledgements
+      },
+      'clock' : ZabbixValue.definition('Created', 'string', time.toIso8601String(),
           false),
       'ns' : ZabbixValue.definition('Created (nanoseconds)', 'string',
           event['ns'], false),
@@ -50,10 +77,14 @@ class ZabbixEvent extends ZabbixChild {
           event['objectid'], false),
       'source' : ZabbixValue.definition('Type', 'string', source, false),
       'value' : ZabbixValue.definition('Value', 'string', eventValue, false),
-      'value_changed' : ZabbixValue.definition('Value Changed', 'number',
-          event['value_changed'], false),
       AcknowledgeEvent.pathName : AcknowledgeEvent.definition()
     };
+
+    if (event['value_changed'] != null) {
+      ret['value_changed'] = ZabbixValue.definition('Value Changed', 'number',
+          event['value_changed'], false);
+    }
+    return ret;
   }
 
   ZabbixEvent(String path) : super(path);
